e6c4af1842c03624dc5508c408c28d579f7a7331
nicks96432 <nicks96432@gmail.com>
2019-09-18 15:01:01 +0800

    digits

--- Character/Digits.c
+++ Character/Digits.c
@@ -18,39 +18,189 @@ int main(void)
 		int const len = strlen(num);
 		for (i = 0; i < b; i++)
 		{
-			for (j = 0; j < len; j++)
+			if (i == 0)
 			{
-				switch (num[j])
+				for (j = 0; j < len; j++)
 				{
-				case '0':
-				case '2':
-				case '3':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-					printf(" ");
-					for (k = 0; k < ((a % 2 == 0) ? (a / 2) : ((a / 2) + 1)); k++)
+					switch (num[j])
 					{
-						printf("%c", num[j]);
-					}
-					printf(" ");
-					break;
-				case '1':
-				case '4':
-					for (k = 0; k < a; k++)
-					{
+					case '0':
+					case '2':
+					case '3':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
 						printf(" ");
+						for (k = 0; k < a - 3; k++)
+						{
+							printf("%c", num[j]);
+						}
+						printf("  ");
+						break;
+					case '1':
+					case '4':
+						for (k = 0; k < a; k++)
+						{
+							printf(" ");
+						}
+						break;
+					default:
+						break;
 					}
-					break;
-				default:
-					break;
+				}
+			}
+			else if (i == b / 2)
+			{
+				for (j = 0; j < len; j++)
+				{
+					switch (num[j])
+					{
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '8':
+					case '9':
+						printf(" ");
+						for (k = 0; k < a - 3; k++)
+						{
+							printf("%c", num[j]);
+						}
+						printf("  ");
+						break;
+					case '0':
+					case '1':
+					case '7':
+						for (k = 0; k < a; k++)
+						{
+							printf(" ");
+						}
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			else if (i < b / 2)
+			{
+				for (j = 0; j < len; j++)
+				{
+					switch (num[j])
+					{
+					case '1':
+					case '2':
+					case '3':
+						for (k = 0; k < a - 2; k++)
+						{
+							printf(" ");
+						}
+						printf("%c", num[j]);
+						printf(" ");
+						break;
+					case '5':
+					case '6':
+						printf("%c", num[j]);
+						for (k = 0; k < a - 1; k++)
+						{
+							printf(" ");
+						}
+						break;
+					case '0':
+					case '4':
+					case '7':
+					case '8':
+					case '9':
+						printf("%c", num[j]);
+						for (k = 0; k < a - 3; k++)
+						{
+							printf(" ");
+						}
+						printf("%c", num[j]);
+						printf(" ");
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			else if (i > b / 2 && i < b - 1)
+			{
+				for (j = 0; j < len; j++)
+				{
+					switch (num[j])
+					{
+					case '1':
+					case '3':
+					case '4':
+					case '5':
+					case '7':
+					case '9':
+						for (k = 0; k < a - 2; k++)
+						{
+							printf(" ");
+						}
+						printf("%c ", num[j]);
+						break;
+					case '2':
+						printf("%c", num[j]);
+						for (k = 0; k < a - 1; k++)
+						{
+							printf(" ");
+						}
+						break;
+					case '0':
+					case '6':
+					case '8':
+						printf("%c", num[j]);
+						for (k = 0; k < a - 3; k++)
+						{
+							printf(" ");
+						}
+						printf("%c ", num[j]);
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			else
+			{
+				for (j = 0; j < len; j++)
+				{
+					switch (num[j])
+					{
+					case '0':
+					case '2':
+					case '3':
+					case '5':
+					case '6':
+					case '8':
+						printf(" ");
+						for (k = 0; k < a - 3; k++)
+						{
+							printf("%c", num[j]);
+						}
+						printf("  ");
+						break;
+					case '1':
+					case '4':
+					case '7':
+					case '9':
+						for (k = 0; k < a; k++)
+						{
+							printf(" ");
+						}
+						break;
+					default:
+						break;
+					}
 				}
 			}
 			printf("\n");
 		}
-
 #endif
 	}
 	return 0;
