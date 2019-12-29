8cdb65f65077f709b955ece34fb9476cf22018d7
nicks96432 <nicks96432@gmail.com>
2019-12-29 10:34:45 +0800

    改一下Sort Dates， 怕它爆掉

--- Stdlib__Preprocessing/Sort__Dates.c
+++ Stdlib__Preprocessing/Sort__Dates.c
@@ -3,23 +3,23 @@
 #define MAX_N 1000000
 int comp(const void *x, const void *y)
 {
-	return (*(unsigned long long *)x) - (*(unsigned long long *)y);
+	return (*(long long *)x) - (*(long long *)y) > 0 ? 1 : -1;
 }
-static unsigned long long dates[MAX_N];
+static long long dates[MAX_N];
 int main(void)
 {
 	int n;
-	unsigned long long year, month, day;
+	long long year, month, day;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 	{
-		scanf("%llu%llu%llu", &year, &month, &day);
+		scanf("%lld%lld%lld", &year, &month, &day);
 		dates[i] = day + month * 100 + year * 10000;
 	}
 	qsort(dates, n, sizeof(dates[0]), comp);
 	for (int i = 0; i < n; i++)
 	{
-		printf("%llu %llu %llu\n", dates[i] / 10000, (dates[i] % 10000 - dates[i] % 100) / 100,
+		printf("%lld %lld %lld\n", dates[i] / 10000, (dates[i] % 10000 - dates[i] % 100) / 100,
 			   dates[i] % 100);
 	}
 	return 0;
