2253ecce8d0bf8e1b61bcc6084ec06ba2d73e562
nicks96432 <nicks96432@gmail.com>
2020-01-04 20:01:36 +0800

    Stack Rectangles, Again

--- Exam__2018/Stack__Rectangles__Again.c
+++ Exam__2018/Stack__Rectangles__Again.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 #define MAX_RECTANGLES 200
-int n;
-int used[MAX_RECTANGLES] = {};
 typedef struct Rectangle
 {
 	int width;
@@ -13,74 +11,64 @@ typedef struct Solution
 	int layer;
 	int sum;
 } solution;
-/* a 在 b 上 */
+/* a 可以放在 b 上 */
 bool check(rectangle a, rectangle b)
 {
-#ifdef LARGEONSMALL
-	return (a.length <= b.length && a.width <= b.width) || (a.length <= b.width && a.width <= b.length);
-#elif defined SMALLONLARGE
-	return (a.length >= b.length && a.width >= b.width) || (a.length >= b.width && a.width >= b.length);
+#ifdef SMALLONLARGE
+	return (a.length <= b.length && a.width <= b.width) ||
+		   (a.length <= b.width && a.width <= b.length);
+#elif defined LARGEONSMALL
+	return (a.length >= b.length && a.width >= b.width) ||
+		   (a.length >= b.width && a.width >= b.length);
 #endif
 }
-void print__solution(solution sol)
+void printSolution(solution sol)
 {
 	printf("%d %d\n", sol.layer, sol.sum);
 	return;
 }
-solution better__solution(solution a, solution b)
+solution betterSolution(solution a, solution b)
 {
-#ifdef LARGEONSMALL
-	if (a.layer > b.layer || (a.layer == b.layer && a.sum > b.sum))
-	{
-		return a;
-	}
-	else
-	{
-		return b;
-	}
-#elif defined SMALLONLARGE
-	if (a.layer < b.layer || (a.layer == b.layer && a.sum < b.sum))
-	{
-		return a;
-	}
-	else
-	{
-		return b;
-	}
-
+#ifdef MAXAREASUM
+	return (a.layer > b.layer || (a.layer == b.layer && a.sum > b.sum)) ? a : b;
+#elif defined MINAREASUM
+	return (a.layer > b.layer || (a.layer == b.layer && a.sum < b.sum)) ? a : b;
 #endif
 }
-solution stack(rectangle rectangles[], int now, rectangle bottom, solution sol)
+solution stack(int now, int n, rectangle top, rectangle rectangles[], solution sol)
 {
 	if (now >= n)
 	{
 		return sol;
 	}
-	if (check(rectangles[now], bottom))
+	if (check(rectangles[now], top))
 	{
-		solution new__sol = {sol.layer + 1, sol.sum + now};
-		return better__solution(stack(rectangles, now + 1, rectangles[now], new__sol), stack(rectangles, now + 1, bottom, sol));
+		solution new__sol = {sol.layer + 1,
+							 sol.sum + rectangles[now].width * rectangles[now].length};
+		return betterSolution(stack(now + 1, n, rectangles[now], rectangles, new__sol),
+							  stack(now + 1, n, top, rectangles, sol));
 	}
 	else
 	{
-		return stack(rectangles, now + 1, bottom, sol);
+		return stack(now + 1, n, top, rectangles, sol);
 	}
 }
 int main(void)
 {
-	rectangle rectangles[MAX_RECTANGLES];
+	int n;
 	scanf("%d", &n);
+	rectangle rectangles[MAX_RECTANGLES];
 	int i;
 	for (i = 0; i < n; i++)
 	{
-		scanf("%d%d", &rectangles[i].width, &rectangles[i].length);
+		scanf("%d%d", &rectangles[i].length, &rectangles[i].width);
 	}
-#ifdef LARGEONSMALL
+#ifdef SMALLONLARGE
 	rectangle bottom = {0x7fffffff, 0x7fffffff};
-#elif SMALLONLARGE
+#elif defined LARGEONSMALL
 	rectangle bottom = {0x80000000, 0x80000000};
 #endif
-	solution ans = {0, 0};
-	print__solution(stack(rectangles, 0, bottom, ans));
+	solution zero = {0, 0};
+	printSolution(stack(0, n, bottom, rectangles, zero));
 	return 0;
 }
